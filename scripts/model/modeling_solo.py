b6301b654adba0846e4dd0a6106da49aeae1f358
lalala <99693356+zyy98123@users.noreply.github.com>
2024-10-27 15:39:40 +0800

    Update modeling_solo.py

--- scripts/model/modeling_solo.py
+++ scripts/model/modeling_solo.py
@@ -129,6 +129,7 @@ class SoloConfig(PretrainedConfig):
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
         self.sliding_window = sliding_window
+        self.head_dim = kwargs.get('head_dim', 64)
 
         # for backward compatibility
         if num_key_value_heads is None:
