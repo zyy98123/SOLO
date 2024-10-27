3026a3704dce3b963e3e118711582b9a70f8baaa
lalala <99693356+zyy98123@users.noreply.github.com>
2024-10-27 13:46:16 +0800

    Update evolution.py

--- evolution.py
+++ evolution.py
@@ -89,7 +89,7 @@ def prepare_inputs(inputs: list, device: str):
     else:
         vision_patches = None
     vision_patch_indices = torch.Tensor(np.array(vision_patch_indices)).long()
-    vision_patch_indices = torch.clamp(vision_patch_indices, min=0, max=vision_embeds.shape[0] - 1)
+    vision_patch_indices = torch.clamp(vision_patch_indices, min=0, max=vision_patches.shape[0] - 1)
 
 
     # Replace any -1 with valid index pointing to a dummy zero embedding (if needed)
